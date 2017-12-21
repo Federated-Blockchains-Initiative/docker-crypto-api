32e91d838b9f1700ae4569f2422206d5ed0a11f7
Stephen Erisman <stephen.erisman@wonderlic.com>
2017-12-21 15:28:50 -0600

    re-subscribe on a re-connection

--- quotes.js
+++ quotes.js
@@ -10,7 +10,20 @@ const _cache = {
 const socket = require('socket.io-client')('https://streamer.cryptocompare.com/');
 
 const subscription = ['2~Coinbase~BTC~USD', '2~Coinbase~BCH~USD', '2~Coinbase~ETH~USD', '2~Coinbase~LTC~USD'];
-socket.emit('SubAdd', {subs: subscription});
+
+socket.on('connect', function() {
+  console.log('Socket Connected!');
+  socket.emit('SubAdd', {subs: subscription});
+});
+
+socket.on('disconnect', function() {
+  console.log('Socket Disconnected!');
+});
+
+socket.on('reconnect', function() {
+  console.log('Socket Reconnected!');
+  socket.emit('SubAdd', {subs: subscription});
+});
 
 socket.on('m', function(message) {
   // e.g. 2~Coinbase~BTC~USD~1~18990~1513646467~0.15758392~2992.5186408~28890362~18310.834604059848~344920976.41323745~ce9
@@ -25,7 +38,9 @@ socket.on('m', function(message) {
 
 function priceUpdated(symbol, price) {
   const cachedObj = _cache[symbol];
-  //console.log(symbol, price, price > cachedObj.price ? 'UP' : 'DOWN');
+  // if (price !== cachedObj.price) {
+  //   console.log(symbol, price, price > cachedObj.price ? 'UP' : 'DOWN');
+  // }
   cachedObj.price = price;
 }
 
